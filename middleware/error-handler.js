e379d512fa29d75764b123fc1b9b8e89c766c667
[SaadMu7ammad] <[saadmu7ammad711711@gmail.com]>
2023-11-27 21:38:04 +0200

    tracing error by stack property

--- middleware/error-handler.js
+++ middleware/error-handler.js
@@ -1,4 +1,6 @@
 const { CustomAPIError } = require('../errors');
+require('dotenv').config();
+
 const { StatusCodes } = require('http-status-codes');
 const errorHandlerMiddleware = (err, req, res, next) => {
   // console.log(err.message);
@@ -9,22 +11,29 @@ const errorHandlerMiddleware = (err, req, res, next) => {
   if (err instanceof CustomAPIError) {
     console.log('errorHandlerMiddleware1');
 
-    return res.status(err.statusCode).json({ msg: err.message });
+    return res
+      .status(err.statusCode)
+      .json({
+        msg: err.message,
+        stack: process.env.NODE_ENV === 'production' ? null : err.stack,
+      });
   }
   console.log('errorHandlerMiddleware2');
   if (err.code && err.code === 11000) {
-    return res
-      .status(StatusCodes.BAD_REQUEST)
-      .json({
-        msg: `${err.keyValue['email']} cant duplicated for registering again `,
-      });
+    return res.status(StatusCodes.BAD_REQUEST).json({
+      msg: `${err.keyValue['email']} cant duplicated for registering again `,
+      stack: process.env.NODE_ENV === 'production' ? null : err.stack,
+    });
   }
   //  return res.status(customError.statusCode).json({ err })//;to show and handle the errors on postman to work with it
   if (err.value && err.name === 'CastError') {
     customError.statusCode = StatusCodes.BAD_REQUEST;
     customError.msg = `${err.value} not a valid id for tasks`;
-  } 
-  return res.status(customError.statusCode).json({ msg: customError.msg });
+  }
+  return res.status(customError.statusCode).json({
+    msg: customError.msg,
+    stack: process.env.NODE_ENV === 'production' ? null : err.stack,
+  });
 };
 
 module.exports = errorHandlerMiddleware;
