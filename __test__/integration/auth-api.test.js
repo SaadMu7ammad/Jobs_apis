b6839bb739d06e371e48d412bd0f2d2f5102ce41
[SaadMu7ammad] <[saadmu7ammad711711@gmail.com]>
2023-12-01 19:45:48 +0200

    wrapping with outer describe

--- __test__/integration/auth-api.test.js
+++ __test__/integration/auth-api.test.js
@@ -10,79 +10,81 @@ afterAll(async () => {
 beforeAll(async () => {
   await User.findOneAndDelete({ email: 'newtest@gmail.com' });
 });
-describe('api testing for register user ', () => {
-  it('should throw error when user is registered before', async () => {
-    const existingObj = {
-      name: 'test',
-      email: 'test@gmail.com',
-      password: 'saad12',
-    };
-    const res = await request(startApp)
-      .post('/api/v1/auth/register')
-      .send(existingObj);
-    expect(res.status).toBe(400);
-    expect(res.body).toHaveProperty(
-      'msg',
-      `${existingObj.email} cant duplicated for registering again `
-    );
+describe('api teseing for auth.service', () => {
+  describe('api testing for register user ', () => {
+    it('should throw error when user is registered before', async () => {
+      const existingObj = {
+        name: 'test',
+        email: 'test@gmail.com',
+        password: 'saad12',
+      };
+      const res = await request(startApp)
+        .post('/api/v1/auth/register')
+        .send(existingObj);
+      expect(res.status).toBe(400);
+      expect(res.body).toHaveProperty(
+        'msg',
+        `${existingObj.email} cant duplicated for registering again `
+      );
+    });
+    it('should throw error when any parameters are missing', async () => {
+      const existingObj = {
+        name: 'test',
+        email: 'test@gmail.com',
+      };
+      const res = await request(startApp)
+        .post('/api/v1/auth/register')
+        .send(existingObj);
+      expect(res.status).toBe(400);
+      expect(res.body).toHaveProperty('msg', `empty inputs`);
+    });
+    it('happy scenario', async () => {
+      const newObj = {
+        name: 'newtest',
+        email: 'newtest@gmail.com',
+        password: 'newtest',
+      };
+      const res = await request(startApp)
+        .post('/api/v1/auth/register')
+        .send(newObj);
+      expect(res.status).toBe(201);
+      expect(res.body.user).toHaveProperty('name', newObj.name);
+    });
   });
-  it('should throw error when any parameters are missing', async () => {
-    const existingObj = {
-      name: 'test',
-      email: 'test@gmail.com',
-    };
-    const res = await request(startApp)
-      .post('/api/v1/auth/register')
-      .send(existingObj);
-    expect(res.status).toBe(400);
-    expect(res.body).toHaveProperty('msg', `empty inputs`);
-  });
-  it('happy scenario', async () => {
-    const newObj = {
-      name: 'newtest',
-      email: 'newtest@gmail.com',
-      password: 'newtest',
-    };
-    const res = await request(startApp)
-      .post('/api/v1/auth/register')
-      .send(newObj);
-    expect(res.status).toBe(201);
-    expect(res.body.user).toHaveProperty('name', newObj.name);
-  });
-});
 
-describe('api testing for auth user ', () => {
-  it('should throw error when the user not founded', async () => {
-    const existingObj = {
-      email: 'notFound@gmail.com',
-      password: 'notnewtestnotFound',
-    };
-    const res = await request(startApp)
-      .post('/api/v1/auth/login')
-      .send(existingObj);
-    expect(res.status).toBe(401);
-    expect(res.body).toHaveProperty('msg', `invalid credentials`);
-  });
-  it('should throw error when any parameters are missing', async () => {
-    const existingObj = {
-      email: 'newtest@gmail.com',
-      //   password: 'newtest',
-    };
-    const res = await request(startApp)
-      .post('/api/v1/auth/login')
-      .send(existingObj);
-    expect(res.status).toBe(400);
-    expect(res.body).toHaveProperty('msg', `empty inputs`);
-  });
-  it('should throw error when the password is incorrect', async () => {
-    const existingObj = {
-      email: 'newtest@gmail.com',
-      password: 'notnewtest',
-    };
-    const res = await request(startApp)
-      .post('/api/v1/auth/login')
-      .send(existingObj);
-    expect(res.status).toBe(401);
-    expect(res.body).toHaveProperty('msg', `not correct password`);
+  describe('api testing for auth user ', () => {
+    it('should throw error when the user not founded', async () => {
+      const existingObj = {
+        email: 'notFound@gmail.com',
+        password: 'notnewtestnotFound',
+      };
+      const res = await request(startApp)
+        .post('/api/v1/auth/login')
+        .send(existingObj);
+      expect(res.status).toBe(401);
+      expect(res.body).toHaveProperty('msg', `invalid credentials`);
+    });
+    it('should throw error when any parameters are missing', async () => {
+      const existingObj = {
+        email: 'newtest@gmail.com',
+        //   password: 'newtest',
+      };
+      const res = await request(startApp)
+        .post('/api/v1/auth/login')
+        .send(existingObj);
+      expect(res.status).toBe(400);
+      expect(res.body).toHaveProperty('msg', `empty inputs`);
+    });
+    it('should throw error when the password is incorrect', async () => {
+      const existingObj = {
+        email: 'newtest@gmail.com',
+        password: 'notnewtest',
+      };
+      const res = await request(startApp)
+        .post('/api/v1/auth/login')
+        .send(existingObj);
+      expect(res.status).toBe(401);
+      expect(res.body).toHaveProperty('msg', `not correct password`);
+    });
   });
 });
